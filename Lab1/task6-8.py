ed6b7ce5a666431d001a42f129c32c7bc58c1150
Se2up <alex.leeesnikov@gmail.com>
2026-04-22 11:34:31 +0300

    Поиск минимальных рациональных чисел

--- Lab1/task6-8.py
+++ Lab1/task6-8.py
@@ -12,5 +12,20 @@ def find_max_float(text: str) -> float:
 
     return max_num
 
-text = str(input("Введите вещественные числа: "))
-print(find_max_float(text))
+def find_min_rational(text: str) -> float:
+    numbers = text.split()
+    min_num = float('inf')
+
+    for item in numbers:
+        if '/' in item:
+            try:
+                num = float(item.split('/')[0]) / float(item.split('/')[1])
+                if num < min_num:
+                    min_num = num
+            except:
+                continue
+
+    return min_num
+
+text = str(input("Введите дробные числа: "))
+print(find_min_rational(text))
