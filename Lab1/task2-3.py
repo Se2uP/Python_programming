713c2bf06dd351c655dbb7baacb321f8b3756301
Se2up <alex.leeesnikov@gmail.com>
2026-04-22 10:33:38 +0300

    Проверка на палиндром на строчной латинице

--- Lab1/task2-3.py
+++ Lab1/task2-3.py
@@ -5,6 +5,15 @@ def count_russian_letters(text: str) -> int:
             count += 1
     return count
 
+def is_latin_palindrome(text: str) -> bool:
+    filtered = ""
+
+    for char in text:
+        if 'a' <= char <= 'z':  # только строчные латинские
+            filtered += char
+
+    return filtered == filtered[::-1]
+
 if __name__ == "__main__":
-    text = str(input("Введите текст на русском языке: "))
-    print(count_russian_letters(text))
+    text = str(input("Введите текст: "))
+    print(is_latin_palindrome(text))
