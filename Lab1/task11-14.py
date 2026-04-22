0fc8ad5c3d347726dfd350c8c50e9e7ef30bf9d8
Se2up <alex.leeesnikov@gmail.com>
2026-04-22 12:33:02 +0300

    Сортировка по отклонению ASCII

--- Lab1/task11-14.py
+++ Lab1/task11-14.py
@@ -16,6 +16,16 @@ def count_vowels_and_consonants_diff(s: str) -> int:
 def sort_by_vowel_consonant_diff(strings: list) -> list:
     return sorted(strings, key=count_vowels_and_consonants_diff)
 
+def avg_ascii(s: str) -> float:
+    if len(s) == 0:
+        return 0
+    return sum(ord(ch) for ch in s) / len(s)
+
+
+def sort_by_ascii_deviation(strings: list) -> list:
+    first_avg = avg_ascii(strings[0])
+    return sorted(strings, key=lambda s: abs(avg_ascii(s) - first_avg))
+
 n = int(input("Сколько строк? "))
 strings = [input() for _ in range(n)]
-print(sort_by_vowel_consonant_diff(strings))
+print(sort_by_ascii_deviation(strings))
